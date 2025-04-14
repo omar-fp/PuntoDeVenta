7c2262b778e8e9bcbcd61fde6cdc82d6d25f6ac4
Urion00 <fatimaarellano58@gmail.com>
2025-04-13 21:07:33 -0500

    Controlador vista pantalla principal e integre a vista pantalla principal

---
+++ src/controlador/ControladorVistaPantallaPrincipal.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+import vista.VistaPantallaPrincipal;
+/**
+ *
+ * @author L13 YOGA
+ */
+public class ControladorVistaPantallaPrincipal {
+    VistaPantallaPrincipal VPP;
+    public ControladorVistaPantallaPrincipal(VistaPantallaPrincipal VPP){
+        this.VPP = VPP;
+        this.VPP.setVisible(true);
+    }
+}
--- src/vista/VistaPantallaPrincipal.java
+++ src/vista/VistaPantallaPrincipal.java
@@ -5,10 +5,25 @@
  */
 package vista;
 
+import java.awt.Color;
+import java.awt.Cursor;
+import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
 /**
  *
  * RikaSpark320
  */
-public class VistaPantallaPrincipal {
+public class VistaPantallaPrincipal extends JFrame{
+
+    public VistaPantallaPrincipal() {
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(1200, 1800);
+        this.getContentPane().setBackground(new Color(199,150,242));
+        this.setTitle("Pantalla principal");
+        this.setLocationRelativeTo(null);
+        this.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        this.setResizable(false);
+    }
     
 }
